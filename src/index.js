e736e924bc335fe3e01393251d585ecb574fbb4e
zygotic4 <107277523+zygotic4@users.noreply.github.com>
2023-10-09 10:27:23 +0000

    done

--- src/index.js
+++ src/index.js
@@ -14,26 +14,26 @@ const clientGoogle = new google.auth.JWT(
 clientGoogle.authorize((err, tokens) => {
   if (err) return console.log(err);
   console.log('connection successful');
-  // gsrun(clientGoogle);
+  gsrun(clientGoogle);
 });
 
-// const gsrun = async (client) => {
-//   const gsapi = google.sheets({version:'v4', auth: client});
-//   const options = {
-//     spreadsheetId: '1BXbjFypGLNftVGMcHsMnf5TVvaUksS7Y2oZBir7DPVo',
-//     range: 'Sheet1!A1:B4'
-//   };
-//   let data = await gsapi.spreadsheets.values.get(options);
-//   let dataArray = data.data.values;
-//   let newDataArray = dataArray.map((row) => {
-//     row.push(row[0] + '-' + row[1]);
-//     return row
-//   });
-//   const updateOptions = {
-//     spreadsheetId: '1BXbjFypGLNftVGMcHsMnf5TVvaUksS7Y2oZBir7DPVo',
-//     range: 'Sheet1!E1',
-//     valueInputOption: 'USER_ENTERED',
-//     resource: { values: newDataArray }
-//   };
-//   let response = await gsapi.spreadsheets.values.update(updateOptions);
-// };
+const gsrun = async (client) => {
+  const gsapi = google.sheets({version:'v4', auth: client});
+  const options = {
+    spreadsheetId: '1BXbjFypGLNftVGMcHsMnf5TVvaUksS7Y2oZBir7DPVo',
+    range: 'Sheet1!A1:B4'
+  };
+  let data = await gsapi.spreadsheets.values.get(options);
+  let dataArray = data.data.values;
+  let newDataArray = dataArray.map((row) => {
+    row.push(row[0] + '-' + row[1]);
+    return row
+  });
+  const updateOptions = {
+    spreadsheetId: '1BXbjFypGLNftVGMcHsMnf5TVvaUksS7Y2oZBir7DPVo',
+    range: 'Sheet1!E1',
+    valueInputOption: 'USER_ENTERED',
+    resource: { values: newDataArray }
+  };
+  let response = await gsapi.spreadsheets.values.update(updateOptions);
+};
